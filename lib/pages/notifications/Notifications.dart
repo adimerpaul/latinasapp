b45fc8405f0670327a3a1e9f2319e4989f59e49c
adimerpaul <adimer101@gmail.com>
2024-04-26 06:58:36 -0400

    Modificado modifcar para pedidio

--- lib/pages/notifications/Notifications.dart
+++ lib/pages/notifications/Notifications.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import '../../globals.dart' as globals;
@@ -12,6 +13,7 @@ class Notifications extends StatefulWidget {
 
 class _NotificationsState extends State<Notifications> {
   List books = [];
+  var total = 0.0;
   @override
   void initState() {
     booksGet();
@@ -21,57 +23,153 @@ class _NotificationsState extends State<Notifications> {
     var booksBox = await Hive.openBox<Book>('books');
     setState(() {
       books = booksBox.values.toList();
+      total = 0.0;
       books.forEach((element) {
-        print(element.name);
+        total += element.price * element.quantity;
       });
     });
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: books.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(books[index].name,
-                style: TextStyle(fontWeight: FontWeight.bold,
-                  fontSize: 14,
-                  color: Colors.black,
-                )
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: books.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(books[index].name,
+                      style: TextStyle(fontWeight: FontWeight.bold,
+                        // fontSize: 14,
+                        color: Colors.black,
+                      )
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(books[index].price.toString() + ' Bs',
+                              style: TextStyle(
+                                // fontSize: 12,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold,
+                              )
+                          ),
+                          Text('Total: ' + (books[index].price * books[index].quantity).toString() + ' Bs',
+                              style: TextStyle(
+                                // fontSize: 12,
+                                color: Colors.redAccent,
+                                fontWeight: FontWeight.bold,
+                              )
+                          ),
+                        ],
+                      ),
+                      Row(
+                        //row que están uno a cada lado
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              IconButton(
+                                visualDensity: VisualDensity(horizontal: -4, vertical: -4),
+                                icon: Icon(Icons.remove),
+                                iconSize: 20,
+                                color: Colors.grey,
+                                onPressed: () async {
+                                  var booksBox = await Hive.openBox<Book>('books');
+                                  if (books[index].quantity > 1) {
+                                    books[index].quantity--;
+                                    booksBox.put(books[index].id.toString(), books[index]);
+                                    booksGet();
+                                  }
+                                },
+                              ),
+                              Text(books[index].quantity.toString(),
+                                style: TextStyle(
+                                  // fontSize: 12,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              IconButton(
+                                visualDensity: VisualDensity(horizontal: -4, vertical: -4),
+                                icon: Icon(Icons.add),
+                                iconSize: 20,
+                                color: Colors.grey,
+                                onPressed: () async {
+                                  var booksBox = await Hive.openBox<Book>('books');
+                                  books[index].quantity++;
+                                  booksBox.put(books[index].id.toString(), books[index]);
+                                  booksGet();
+                                },
+                              ),
+                            ],
+                          ),
+                          //icono de trash
+                          IconButton(
+                            visualDensity: VisualDensity(horizontal: -4, vertical: -4),
+                            icon: Icon(Icons.delete_outline),
+                            iconSize: 20,
+                            color: Colors.red,
+                            onPressed: () async {
+                              var booksBox = await Hive.openBox<Book>('books');
+                              booksBox.delete(books[index].id.toString());
+                              booksGet();
+                            },
+                          ),
+                        ],
+                      )
+
+                    ],
+                  ),
+                  leading: Image.network(
+                    globals.API_BACK +
+                        'uploads/thumb_' +
+                        books[index].image,
+                    fit: BoxFit.cover,
+                    width: 50, // Ancho deseado de la imagen
+                    // height: 100, // Alto deseado de la imagen
+                  ),
+                );
+              },
             ),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(books[index].price.toString() + ' Bs',
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.grey,
-                      fontWeight: FontWeight.bold,
-                    )
-                ),
-                Row(
+          ),
+          Container(
+            width: double.infinity,
+            height: 50,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Navigator.pushNamed(context, '/checkout');
+                },
+                child: Row(
+                  //center
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text('Cantidad: ' + books[index].quantity.toString(),
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.grey,
-                          fontWeight: FontWeight.bold,
-                        )
+                    //icono
+                    Icon(Icons.shopping_cart, color: Colors.white),
+                    Text(
+                      'Pagar',
+                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    Text(
+                      '  ' + total.toString() + ' Bs',
+                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 15),
                     ),
                   ],
-                )
-              ],
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                ),
+              ),
             ),
-            leading: Image.network(
-              globals.API_BACK +
-                  'uploads/thumb_' +
-                  books[index].image,
-              fit: BoxFit.cover,
-              width: 50, // Ancho deseado de la imagen
-              height: 100, // Alto deseado de la imagen
-            ),
-          );
-        },
+          ),
+          const SizedBox(height: 35),
+        ]
       ),
     );
   }
